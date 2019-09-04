ca97e1b895cda0039e97caa528e5931dc15b72c2
HarryMWinters <harrymcwinters@gmail.com>
2019-09-04 10:33:47 -0700

    Add solution template II

---
+++ _solution-template/README.md
@@ -0,0 +1,18 @@
+# Challange Name
+
+URL - http://...
+
+Lorem ipsim dolor
+
+## Function Description
+
+Lorem ipsum dolor
+
+## Constraints
+
+- foo <= bar
+- erc.
+
+## Output Format
+
+A lorem ispum dolor data struct.
---
+++ _solution-template/solution.py
@@ -0,0 +1,13 @@
+def func(param1, param2):
+    """
+    Return a value that solves the challange.
+
+    Args:
+        param1: A(n) type(param1), explanation...
+        price2: A(n) type(param2), explanation...
+    Returns:
+       A solution of type type(solution)
+    Raises:
+        (someTypeOf)Error: if things go wrong.
+    """
+    pass
