55d8d5f94550229a9281adb6c30215385211d41e
HarryMWinters <harrymcwinters@gmail.com>
2019-09-19 14:19:53 -0700

    Alternating Characters: red

---
+++ alternating-characters/solution.py
@@ -0,0 +1,13 @@
+def alternatingCharacters(string):
+    """
+    The number of deletions required 
+    so that no two adjacent characters are the same.
+
+    Args:
+        string: A string of "A"s and "B"s
+    Returns:
+       An non-negative integer.
+    Raises:
+        (someTypeOf)Error: if things go wrong.
+    """
+    return -1
