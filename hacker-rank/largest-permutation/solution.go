83fbc4b6cf99ae7139a20f77370a3e360cbd5dd8
HarryMWinters <harrymcwinters@gmail.com>
2019-10-14 19:37:09 -0700

    Remove debugging statements from largest permutation

--- hacker-rank/largest-permutation/solution.go
+++ hacker-rank/largest-permutation/solution.go
@@ -12,21 +12,15 @@ func largestPermutation(k int32, arr []int32) []int32 {
 }
 
 func _swap(arr []int32) []int32 {
-	// fmt.Printf("In swap %d\n", arr)
 	max := len(arr)
 	noPreviousMisMatch := true
 	for i, val := range arr {
 		if val != int32(max-i) && noPreviousMisMatch {
-			// fmt.Printf("Found mismatch on %d\n", val)
 			// The value is too low and should be
 			// swapped with the highest value following.
 			var position int32
-			// fmt.Printf("Examing %d for highest following number\n", arr[i:])
-			// a, b := _max(arr[i:])
-			// fmt.Printf("Found highest number of value %d at position %d\n", a, b)
 			arr[i], position = _max(arr[i:])
 			position = position + int32(i)
-			// fmt.Printf("Swapping: %d and %d\n", arr[i], arr[position])
 			arr[position] = val
 
 			return arr
